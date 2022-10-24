47fa41ee4600ca637920556b2971d27a452e1467
caixtcxt <caixtcxt@foxmail.com>
2022-10-24 20:53:32 +0800

    first commit

---
+++ src/main/java/cn/caixtcxt/GitTest.java
@@ -0,0 +1,18 @@
+package cn.caixtcxt;
+
+/**
+ * @ClassName: GitTest
+ * @Author: XiangtaoCai
+ * @Description:
+ * @Date: 2022/10/24 20:43
+ * @Version: v1.0
+ */
+public class GitTest {
+
+    public static void main(String[] args) {
+
+        System.out.println("Hello git!");
+
+    }
+
+}
