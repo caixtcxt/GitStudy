8e827cb3e40507a546c77c961557ff41432fcb84
Xiangtao Cai <58588309+caixtcxt@users.noreply.github.com>
2022-10-24 22:28:20 +0800

    Update GitTest.java

--- src/main/java/cn/caixtcxt/GitTest.java
+++ src/main/java/cn/caixtcxt/GitTest.java
@@ -19,7 +19,7 @@ public class GitTest {
         System.out.println("masher test");
         System.out.println("hot-fix test");
         System.out.println("push test");
-        System.out.println("pull test");
+        System.out.println("pull test！");
         
     }
 
